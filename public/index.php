d6f6d4ac5a2a62aedddb1bb15238c534faebde41
sleepinglion <admin@sleepinglion.pe.kr>
2017-05-17 10:46:07 +0900

    수정함:        public/css/index.css 	수정함:        public/index.php

--- public/index.php
+++ public/index.php
@@ -165,6 +165,23 @@ try {
 		</tbody>
 	</table>
 	</div>
+  <div id="sl_ad">
+  <?php if (DEBUG): ?>
+  <div style="width:100%;padding-top:10%;background:red">&nbsp;</div>
+  <?php else: ?>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- default_ad -->
+  <ins class="adsbygoogle"
+       style="display:block"
+       data-ad-client="ca-pub-5400903051441488"
+       data-ad-slot="8412654331"
+       data-ad-format="auto"></ins>
+  <script>
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
+  <?php endif ?>
+  </div>
+
 		</div>
   </div>
 	<footer>
